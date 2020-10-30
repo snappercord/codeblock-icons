517d43cd7dc70e965a1b8be56411257a7c31798e
Mulverine Gaming <mulverin3@gmail.com>
2020-10-29 23:33:00 -0600

    This is probably going to break

---
+++ import_langs.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const request = require('request');
+
+request('https://raw.githubusercontent.com/Snapperito/info/a220a176666f7f4128fa00137dc4f80759f78c99/DISCORDHLJS.md', {}, (err, res, body) => {
+    let langs = body.split('\n').slice(3).slice(0, -1);
+
+    langs.forEach((e, i) => {
+        let lang = e.split('|').slice(1).slice(0, -1);
+
+        let classes = lang[1] ? lang[1].split(/\,\s?/) : false;
+
+        if (classes) {
+            classes.forEach((c, j) => classes[j] = c.trim());
+
+            classes = `["${classes.join('", "')}"]`;
+        }
+
+        langs[i] = `"${
+            lang[2] !== '' ? lang[2] : false}": ["${ // Icon
+            lang[0].trim()}", ${ // Display Name
+            classes}]`; // Classes
+    })
+    
+    fs.writeFileSync('langs.scss', `$langs: (${langs.join(', ')});`)
+})
